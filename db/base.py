382301b64e0a269f5a64faf21f7fb3ef2ec05cec
hzloc <huseynz@icloud.com>
2022-08-07 19:38:56 +0100

    revert the changes

--- db/base.py
+++ db/base.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 try:
-    engine = create_engine('postgresql+psycopg2://root:password@localhost:5433/event')
+    engine = create_engine('postgresql+psycopg2://root:password@localhost:5433/bookstore')
 except Error:
     print(Error)
 
